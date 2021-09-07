213ad12af04322786ef81dd364d902cc2f9c38ff
Merlinkoma <ameye.melissa@gmail.com>
2021-09-07 09:23:49 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<?php
+$title = "Konkonmange ?";
+?>
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+</head>
+<body>
+    <h1>Hello Keskonmange</h1>
+</body>
+</html>
